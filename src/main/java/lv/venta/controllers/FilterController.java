8de85ec8c026965fb3c4f1b33be1f0f213fad06c
localuser <localuser@VEA.LV>
2023-05-19 15:52:28 +0300

    New: added controller

---
+++ src/main/java/lv/venta/controllers/FilterController.java
@@ -0,0 +1,22 @@
+package lv.venta.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lv.venta.services.IFilteringService;
+
+@Controller
+public class FilterController {
+
+	@Autowired
+	private IFilteringService filtService;
+	
+	@GetMapping(value = "/info/showAllStudents") //localhost:8080/info/showAllStudents
+	public String getAllStudentsFunc(Model model){
+		model.addAttribute("students", filtService.retrieveAllStudents());
+		return "all-students-page";	//will show all-students-page.html
+	}
+	
+}
